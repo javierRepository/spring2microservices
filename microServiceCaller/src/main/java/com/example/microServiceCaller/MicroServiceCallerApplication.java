3d48d54174131a5e45f900c838bebcada071f3c2
javierRepository <javierantoniomonteropalma@yahoo.es>
2019-03-01 19:49:06 +0100

    Add files via web manually upload
    microserviceCaller

---
+++ microServiceCaller/src/main/java/com/example/microServiceCaller/MicroServiceCallerApplication.java
@@ -0,0 +1,13 @@
+package com.example.microServiceCaller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceCallerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceCallerApplication.class, args);
+	}
+
+}
---
+++ microServiceCaller/src/main/java/com/example/microServiceCaller/controller/CurrencyConversionController.java
@@ -0,0 +1,58 @@
+package com.example.microServiceCaller.controller;
+
+
+import com.example.microServiceCaller.domainobject.CurrencyConversionBean;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+
+/**
+ * Created by javier.montero on 01/03/2019.
+ */
+
+
+@RestController
+public class CurrencyConversionController {
+
+    private static final Logger LOGGER = Logger.getLogger(CurrencyConversionController.class);
+
+    private final Environment environment;
+
+    private StringBuilder completeUrlService;
+
+
+    @Autowired
+    public CurrencyConversionController(Environment environment) {
+        this.environment = environment;
+        this.completeUrlService = new StringBuilder();
+    }
+
+    @GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
+
+
+        String hostService = environment.getProperty("hostService");
+        String serviceCall = environment.getProperty("serviceCall");
+        completeUrlService.append(hostService).append(serviceCall);
+
+        HashMap<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("from", from);
+        uriVariables.put("to", to);
+
+        ResponseEntity<CurrencyConversionBean> responseEntity = new RestTemplate().getForEntity(completeUrlService.toString(), CurrencyConversionBean.class, uriVariables);
+
+        CurrencyConversionBean responseCurrency = responseEntity.getBody();
+
+        return new CurrencyConversionBean(responseCurrency.getId(), from, to, responseCurrency.getConversionMultiple(), quantity, quantity.multiply(responseCurrency.getConversionMultiple()), responseCurrency.getPort());
+
+    }
+
+}
